b7f8a07c50c55f2f7b5d7b3baf93d0e53ec41c5c
Reny Reyes <renyreyes8@hotmail.com>
2022-06-08 18:16:33 -0400

    Added the function layout for crud functionality

---
+++ CertificationsAPI/Repositories/CertificationRepository.cs
@@ -0,0 +1,50 @@
+﻿using CertificationsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks; 
+
+namespace CertificationsAPI.Repositories
+{
+    public class CertificationRepository : ICertificationRepository
+    {
+        // constructor
+        public readonly CertificationContext _context;
+
+        public CertificationRepository(CertificationContext context)
+        {
+            _context = context;
+        }
+
+        // Function to add a new certification
+        public Task<Certification> Create(Certification certification)
+        {
+            return Task.FromResult(certification);
+        }
+        // Function to delete an existing certification
+        public Task<Certification> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        // Function to get all the certifications in the database
+        public Task<IEnumerable<Certification>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        // Function to get a certificate by id
+        public Task<Certification> Get(int id)
+        {
+           throw new NotImplementedException();
+        }
+        // Function to update an existing certificate
+        public Task Update(Certification certification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Certification>> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CertificationsAPI/Repositories/ICertificationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CertificationsAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CertificationsAPI.Repositories
+{
+    public interface  ICertificationRepository
+    {
+        Task<IEnumerable<Certification>> Get();
+        Task<Certification> Get(int id);
+        Task<Certification> Create(Certification certification);
+        Task Update(Certification certification);
+        Task <Certification> Delete(int id);
+
+    }
+}
