836d5e57fa19fe343ece50ded9eb175547cad182
Reny Reyes <renyreyes8@hotmail.com>
2022-06-08 16:45:47 -0400

    Created a certification class in the models folder

---
+++ CertificationsAPI/Certification.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CertificationsAPI
+{
+    public class Certification
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Obtained { get; set; }
+        public string Expired { get; set; }
+        public string Organization { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
