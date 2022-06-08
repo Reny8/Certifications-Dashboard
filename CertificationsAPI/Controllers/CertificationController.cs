74b029d70f0af5dfba642b8ab555592307bc9bd2
Reny Reyes <renyreyes8@hotmail.com>
2022-06-08 19:11:16 -0400

    Modified the program file connected my database

---
+++ CertificationsAPI/Controllers/CertificationController.cs
@@ -0,0 +1,18 @@
+﻿using CertificationsAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CertificationsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CertificationController : ControllerBase
+    {
+        private readonly ICertificationRepository _context;
+
+        public CertificationController(ICertificationRepository context)
+        {
+            _context = context;
+        }
+    }
+}
--- CertificationsAPI/Program.cs
+++ CertificationsAPI/Program.cs
@@ -1,14 +1,28 @@
 using CertificationsAPI.Models;
+using CertificationsAPI.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<CertificationContext>(o => o.UseSqlite("Data source = certifications_database"));
+builder.Services.AddScoped<ICertificationRepository, CertificationRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+builder.Services.AddDbContext<CertificationContext>(opts =>
+                   opts.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=certifications_database; Integrated Security=true",
+                                     sqlServerOptions => sqlServerOptions.CommandTimeout(400)));
 
 var app = builder.Build();
 
