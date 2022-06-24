4182b1aaa6e16d2ffaf1787f60caba86daf5af7d
Reny Reyes <renyreyes8@hotmail.com>
2022-06-24 09:39:31 -0400

    Added a new component folder

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
-  const [certificates, setCertificates] = useState([])
-  useEffect(()=>{
-    getAllCertifications()
-  },[])
+  const [certificates, setCertificates] = useState([]);
+  useEffect(() => {
+    getAllCertifications();
+  }, []);
   async function getAllCertifications() {
     try {
-      let response = await axios.get("https://localhost:7050/api/certification/")
-      console.log(response.data)
+      let response = await axios.get(
+        "https://localhost:7050/api/certification/"
+      );
+      setCertificates(response.data);
     } catch (error) {
-      
+      console.log(error.message);
     }
   }
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ frontend/dashboard/src/Components/DisplayCertificates/DisplayCertificates.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const DisplayCertificates = (props) => {
+  return <div></div>;
+};
+
+export default DisplayCertificates;
