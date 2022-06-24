17305e8ed9b58a59914f7615559bb47bfc9b2f1a
Reny Reyes <renyreyes8@hotmail.com>
2022-06-24 09:37:41 -0400

    Imported axios and got all the certificates from the backend

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 function App() {
+  const [certificates, setCertificates] = useState([])
+  useEffect(()=>{
+    getAllCertifications()
+  },[])
+  async function getAllCertifications() {
+    try {
+      let response = await axios.get("https://localhost:7050/api/certification/")
+      console.log(response.data)
+    } catch (error) {
+      
+    }
+  }
   return (
     <div className="App">
     </div>
