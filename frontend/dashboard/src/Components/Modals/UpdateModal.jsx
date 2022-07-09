b7bbaf41109aef9a678edf207e3bf576773ebd74
Reny Reyes <renyreyes8@hotmail.com>
2022-07-09 17:40:55 -0400

    Adjusted the css for the modal

--- frontend/dashboard/src/Components/Modals/UpdateModal.jsx
+++ frontend/dashboard/src/Components/Modals/UpdateModal.jsx
@@ -3,16 +3,16 @@ import "./UpdateModal.css"
 const UpdateModal = (props) => {
   return (
     <div>
-      <button>
+      <button data-modal-target="#edit-modal">
         <img
           src="https://img.icons8.com/ios-glyphs/60/000000/edit--v1.png"
           alt="Edit Button"
         />
       </button>
-      <div className="edit-modal" id="edit-modal">
+      <div className="modal-template" id="edit-modal">
         <div className="modal-header">
           <h1>Modal Example Title</h1>
-          <button className="close-modal">&times;</button>
+          <button data-close-button className="close-modal">&times;</button>
         </div>
         <div className="modal-body">Body Text....</div>
       </div>
